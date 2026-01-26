6a6d8f20d051ff8fd6ebf28ee718c41ec346c6d5
Kayden Wolf <wokayden@gmail.com>
2026-01-26 14:01:00 -0500

    added tailwind css, starting to make menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,11 @@
+import type { ReactElement } from 'react';
+
+export default function Menu(): ReactElement {
+
+
+    return (
+        <header className="sticky bg-black top-full">
+            header hi!
+        </header>
+    )
+}
