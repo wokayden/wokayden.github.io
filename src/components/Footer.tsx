b50247d621fa65e01894fa849b3e66f3c56f6055
Kayden Wolf <wokayden@gmail.com>
2026-01-27 13:07:58 -0500

    added socialmedia(?) links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,8 +8,18 @@ export default function Footer(): ReactElement {
    return (
       <footer className='bottom-0 flex flex-row content-center justify-end bg-orange-400 fixed inset-x-0 bottom-0'>
          <nav className='flex flex-row content-center justify-between gap-3'>
-            <FaGithub />
-            <FaLinkedin />
+            <a
+               href='https://github.com/wokayden'
+               target='_blank'
+            >
+               <FaGithub />
+            </a>
+            <a
+               href='https://www.linkedin.com/in/kayden-wolf/'
+               target='_blank'
+            >
+               <FaLinkedin />
+            </a>
          </nav>
       </footer>
    );
