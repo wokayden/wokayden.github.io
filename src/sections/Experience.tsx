1c5e8410801010f8671d3a543f62ab863c048fe6
Kayden Wolf <wokayden@gmail.com>
2026-01-27 12:43:14 -0500

    more section structure

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -1,9 +1,37 @@
 import { type ReactElement } from "react";
+import { FaDownload } from "react-icons/fa";
 
 export default function Experience(): ReactElement {
    return (
-      <section className='p-12 h-80 bg-lime-200'>
-         another section
+      <section className='pt-24 pb-24 pl-40 pr-40 bg-lime-200'>
+         <div className='felx flex-col'>
+            <div className='pt-12 pb-12'>
+               <h1 className='w-max text-5xl'>
+                  qualifications
+               </h1>
+            </div>
+            <div className='flex pt-12 pb-12 justify-evenly items-center'>
+               <button className='bg-purple-300 p-4 rounded-xl'>
+                  <span className='flex justify-center gap-3'>
+                     <FaDownload />
+                     <span>resume</span>
+                  </span>
+               </button>
+               <div className='flex flex-col gap-4'>
+                  <div className='flex flex-col gap-4 p-4 border border-black rounded-xl'>
+                     <h1 className='text-2xl'>
+                        directdefense
+                     </h1>
+                     <p className='h-max'>
+                        providing over 4 years of
+                        experience transforming
+                        legacy systems
+                     </p>
+                  </div>
+                  <div className='flex flex-col gap-4 p-4 border border-black rounded-xl'></div>
+               </div>
+            </div>
+         </div>
       </section>
    );
 }
--- src/sections/Splash.tsx
+++ src/sections/Splash.tsx
@@ -2,8 +2,25 @@ import { type ReactElement } from "react";
 
 export default function Splash(): ReactElement {
    return (
-      <main className='p-12 h-80 bg-blue-200'>
-         <h1>UNDER CONSTRUCTION</h1>
+      <main className='p-24 bg-blue-300'>
+         <div className='flex w-full justify-evenly items-center'>
+            <div className='bg-red-50 p-16 rounded-full min-h-100 flex items-center'>
+               <div className='w-max h-max'>
+                  an image will go here
+               </div>
+            </div>
+            <div className='w-max flex flex-col'>
+               <h1 className='w-max p-4 text-7xl'>
+                  some big text
+               </h1>
+               <p className='w-max p-4'>
+                  some smaller text
+               </p>
+               <button className='w-max bg-yellow-200 p-4 mt-4 ml-2 rounded-xl'>
+                  a call to action
+               </button>
+            </div>
+         </div>
       </main>
    );
 }
