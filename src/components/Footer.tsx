0c30d485b20349d135b33aa941a5531b4206dcad
Kayden Wolf <wokayden@gmail.com>
2026-01-27 11:04:06 -0500

    changing prettier formatting

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,16 @@
-import type {ReactElement} from 'react';
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import type { ReactElement } from "react";
+import {
+   FaGithub,
+   FaLinkedin,
+} from "react-icons/fa";
 
 export default function Footer(): ReactElement {
-    return (
-        <footer className="bottom-0 flex flex-row content-center justify-end bg-orange-400 fixed inset-x-0 bottom-0">
-            <nav className="flex flex-row content-center justify-between gap-3">
-                <FaGithub />
-                <FaLinkedin />
-            </nav>
-        </footer>
-    )
+   return (
+      <footer className='bottom-0 flex flex-row content-center justify-end bg-orange-400 fixed inset-x-0 bottom-0'>
+         <nav className='flex flex-row content-center justify-between gap-3'>
+            <FaGithub />
+            <FaLinkedin />
+         </nav>
+      </footer>
+   );
 }
